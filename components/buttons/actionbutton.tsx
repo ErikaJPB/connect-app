b3df6d463e06cae75b52b49db22992a2779506ba
ErikaJPB <erikajpinedab@gmail.com>
2023-09-26 10:04:04 -0500

    added modal to confirm deleting post

--- components/buttons/actionbutton.tsx
+++ components/buttons/actionbutton.tsx
@@ -1,9 +1,8 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { SlOptionsVertical } from "react-icons/sl";
 import { CiEdit } from "react-icons/ci";
 import { MdDeleteOutline } from "react-icons/md";
-
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,6 +18,16 @@ interface ActionButtonProps {
 }
 
 function ActionButton({ postId, userId }: ActionButtonProps) {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const openDeleteModal = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+
   const handleEdit = () => {
     console.log("Edit Post");
   };
@@ -29,6 +38,7 @@ function ActionButton({ postId, userId }: ActionButtonProps) {
 
       if (result.success) {
         console.log("Post deleted successfully");
+        closeDeleteModal();
       }
     } catch (error: any) {
       console.error("Error deleting post", error);
@@ -39,7 +49,7 @@ function ActionButton({ postId, userId }: ActionButtonProps) {
     <>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <button className="cursor-pointer hover:opacity-75 transition">
+          <button className="cursor-pointer transition">
             <SlOptionsVertical
               size={16}
               className="text-black cursor-pointer object-contain"
@@ -49,15 +59,40 @@ function ActionButton({ postId, userId }: ActionButtonProps) {
         <DropdownMenuContent align="end" className="drop-down-menu">
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
           <DropdownMenuItem onClick={handleEdit}>
-            <CiEdit size={25} className="text-black mr-2 h-4 w-4" />
+            <CiEdit size={25} className="drop-down-menu-item" />
             Edit Post
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={handleDelete}>
-            <MdDeleteOutline size={25} className="text-black mr-2 h-4 w-4" />
+          <DropdownMenuItem onClick={openDeleteModal}>
+            <MdDeleteOutline size={25} className="drop-down-menu-item" />
             Delete Post
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+
+      {isDeleteModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="modal-overlay fixed inset-0 bg-black opacity-50"></div>
+          <div className="modal-container bg-white mx-auto p-5 rounded-lg shadow-lg z-50">
+            <h2 className="text-xl font-semibold mb-4">
+              Are you sure you want to delete this post?
+            </h2>
+            <div className="flex justify-end">
+              <button
+                className="px-4 py-2 mr-2 bg-secondary text-gray-900 rounded hover:bg-red-500"
+                onClick={handleDelete}
+              >
+                Yes
+              </button>
+              <button
+                className="px-4 py-2 bg-secondary text-gray-900 rounded hover:bg-gray-500"
+                onClick={closeDeleteModal}
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
